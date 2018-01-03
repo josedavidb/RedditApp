74655f1da08e3a8145369fc0f4ceacf6238fe91b
josedavidb <josedavidb6@gmail.com>
2018-01-03 04:18:11 -0400

    Peticion http de la api de Reddit para los posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { Http } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http, Response} from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  /*Arreglo que va a contener cada uno de los posts*/
+  listaPosts:Object[];
 
+  constructor(public navCtrl: NavController, private http:Http) {
+    this.listaPosts = [];
+
+    /*Hacemos una comunicacion http y una peticion get para extraer la informacion
+    de la API de Reddit */
+    http.get('https://www.reddit.com/r/gaming/.json')
+
+    /*Convertimos lo que obtenemos en un objeto json*/
+    .map((res:Response) => res.json())
+    
+    /*Usamos suscribe para manipular lo obtenido con el Observable y asi agarrar
+    justo lo que necesitamos (cada post) e insertarlo en nuestro arreglo*/
+    .subscribe(res =>
+    res.data.children.forEach( post => this.listaPosts.push(post.data)));
   }
 
 }
